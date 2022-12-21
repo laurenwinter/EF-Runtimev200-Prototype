93e50ba75fe442493420e2df31f2d6b40ed4697b
Lauren <lwinter@esri.com>
2022-12-21 10:00:07 -0800

    Initial Commit

---
+++ EFArcGISv200Proto/EFArcGISv200ProtoApp.swift
@@ -0,0 +1,17 @@
+//
+//  EFArcGISv200ProtoApp.swift
+//  EFArcGISv200Proto
+//
+//  Created by Lauren Winter on 12/21/22.
+//
+
+import SwiftUI
+
+@main
+struct EFArcGISv200ProtoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
