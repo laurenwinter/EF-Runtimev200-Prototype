9036a45a7105ebca8a15b501f61d5a37220ed39b
Lauren <lwinter@esri.com>
2023-01-12 15:20:38 -0800

    Added basemap selector view and required datamodel

---
+++ EFArcGISv200Proto/EFArcGISContent/EFBasemapGalleryView.swift
@@ -0,0 +1,8 @@
+//
+//  EFBasemapGalleryView.swift
+//  EFArcGISv200Proto
+//
+//  Created by Lauren Winter on 1/12/23.
+//
+
+import Foundation
---
+++ EFArcGISv200Proto/RuntimeSamplesCode/MapDataModel.swift
@@ -0,0 +1,8 @@
+//
+//  MapDataModel.swift
+//  EFArcGISv200Proto
+//
+//  Created by Lauren Winter on 1/12/23.
+//
+
+import Foundation
